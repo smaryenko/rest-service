abce4a8f19b9e6bea12550b2155f4f5c00f8a6fb
Stanislav Maryenko <smaryenko@sdl.com>
2016-08-12 15:51:09 +0200

    simple rest service on Spring using Gradle build

--- test/src/test/java/userService/TestBase.java
+++ test/src/test/java/userService/TestBase.java
@@ -4,5 +4,5 @@ public class TestBase {
 
     public static final String SERVICE_URL = "http://localhost:8080";
     public static final String LONG_STRING = "ThisStringContainsNoSpacesAndItsLengthIs255charsBeleiveOrCountItIsStillOnlyEnglishCharsAndNumbersInsideAndNumberOfSymbolsIs255SoYouCanCopyItRightNowAndFindIfTheFieldIsHandlingThe255StringThatIsTypicallMaxLenghtForMostTextFieldsInTCMAPIAtLeastIn2009Version0";
-    public static final String UTF_CHARS = "私はガラスмненевредитذللمنيçanefait☺☻■";
+    public static final String UTF_CHARS = "мненевредитذللمنيçanefait☺☻■";
 }
